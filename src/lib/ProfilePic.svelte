e6945eafc26b224e60c9d148cd23d76925e88eb2
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-04-30 23:53:37 -0700

    Make default image darker in dark mode

--- src/lib/ProfilePic.svelte
+++ src/lib/ProfilePic.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
-  import { profilePic } from "../stores";
+  import { noImgFallback, profilePic } from "../stores";
+
+  $: darken = $profilePic === noImgFallback;
 </script>
 
-<img alt="COVID badge profile" src={$profilePic} />
+<img alt="COVID badge profile" class:darken src={$profilePic} />
 
 <style lang="postcss">
   img {
     padding-top: 0.1rem;
     height: 22.5vh;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    .darken {
+      filter: brightness(65%);
+    }
   }
 </style>
