c27e7e587224a4ecb4d5359167babb245809e4d8
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-05-03 01:07:56 -0700

    Use SSG instead of SSR and remove unused deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import("@sveltejs/kit").Config} */
@@ -11,6 +11,11 @@ const config = {
 
   kit: {
     adapter: adapter(),
+
+    // prerender all pages except those that `export const prerender = false;`
+    prerender: {
+      default: true,
+    },
   },
 };
 
